102765aa5d0ba10292a25ecf2ea208150a9ba551
Arow1497 <fallahi.m.h.97@gmail.com>
2024-11-18 10:05:34 +0330

    Hi

---
+++ .history/app/utils/zarinpalConfig_20241118100206.js
@@ -0,0 +1,17 @@
+export const zarinpalConfig = {
+    MERCHANT_ID: process.env.ZARINPAL_MERCHANT_ID,
+    SANDBOX: process.env.NODE_ENV !== 'production',
+    CALLBACK_URL: process.env.PAYMENT_CALLBACK_URL,
+    API_URLS: {
+      SANDBOX: {
+        REQUEST: 'https://sandbox.zarinpal.com/pg/rest/WebGate/PaymentRequest.json',
+        VERIFY: 'https://sandbox.zarinpal.com/pg/rest/WebGate/PaymentVerification.json',
+        START: 'https://sandbox.zarinpal.com/pg/StartPay/',
+      },
+      PRODUCTION: {
+        REQUEST: 'https://api.zarinpal.com/pg/v4/payment/request.json',
+        VERIFY: 'https://api.zarinpal.com/pg/v4/payment/verify.json',
+        START: 'https://www.zarinpal.com/pg/StartPay/',
+      }
+    }
+  };
